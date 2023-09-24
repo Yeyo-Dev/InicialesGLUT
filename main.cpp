b38856d05244b6f1840175391bc0fb3b7b0611f6
Yeyo Hernandez <76223658+Yeyo-Dev@users.noreply.github.com>
2023-09-24 00:48:14 -0600

    Letra T agregada

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
-//#include <GL/freeglut.h>
-#include <windows.h>
 #include <cstdlib>
 #include <iostream>
-#include <gl\glut.h>
 #include <string.h>
+
+#ifdef _WIN32
+#include <windows.h>
+#include <gl\glut.h>
+#else
+#include <GL/freeglut.h>
+#endif
 
 using namespace std;
 
@@ -66,11 +70,11 @@ void letraA(int x, int y ){
 
 void letraL(int x, int y ){
     lineaV(x,y,16);
-    lineaV(x+4,y+4,12);
+    lineaV(x+6,y+5,11);
     lineaH(x,y,16);
-    lineaH(x+4,y+4,12);
-    lineaH(x,y+16,4);
-    lineaV(x+16,y,4);
+    lineaH(x+6,y+5,10);
+    lineaH(x,y+16,6);
+    lineaV(x+16,y,5);
 }
 
 void letraE(int x, int y ){
@@ -127,13 +131,15 @@ void letraV(int x, int y ){
 }
 
 void letraC(int x, int y) {
-    lineaV(x, y, 16);          // Borde izquierdo vertical
-    lineaH(x, y + 16, 16);     // Borde inferior horizontal
+    lineaV(x, y+2, 12);          // Borde izquierdo vertical
+    lineaDiagonalID(x,y+14,2);
+    lineaH(x+2, y + 16, 12);     // Borde inferior horizontal
+    lineaDiagonalDI(x+14,y+14,2);
 
 
 
     lineaH(x + 4, y + 12, 7);  // Borde inferior interno horizontal
-    lineaV(x+16,y+10,6); //borde vertical hacia arriba de enfrente
+    lineaV(x+16,y+10,4); //borde vertical hacia arriba de enfrente
 
     lineaV(x+11,y+10,2);//borde vertical de en medio hacia enfrente
     lineaH(x + 11, y + 10, 5);
@@ -144,8 +150,10 @@ void letraC(int x, int y) {
     lineaV(x+11,y+4,2);//borde vertical de abajo hacia enfrente
     lineaH(x + 11, y + 6, 5);
 
-    lineaV(x+16,y,6);// borde vertical de abajo hacia el frente
-    lineaH(x,y,16);
+    lineaDiagonalID(x+14,y,2);
+    lineaV(x+16,y+2,4);// borde vertical de abajo hacia el frente
+    lineaDiagonalDI(x,y,2);
+    lineaH(x+2,y,12);
 
 }
 
@@ -208,7 +216,24 @@ void letraM(int x, int y ){
     lineaH(x+11,y,5);
 }
 
-void Letras(void){
+void letraS(int x, int y ){
+}
+
+void letraT(int x, int y ){
+    lineaV(x,y+11,5);
+    lineaH(x,y+16,16);
+
+    lineaV(x+11,y,11);
+    lineaV(x+5,y,11);
+
+    lineaH(x,y+11,5);
+    lineaH(x+11,y+11,5);
+    lineaH(x+5,y,6);
+    lineaV(x+16,y+11,5);
+
+}
+
+void LetrasIniciales(void){
 int fila1=120,fila2=70,fila3=15;
 int columna[] = {10, 27, 44, 61};
 glClear (GL_COLOR_BUFFER_BIT); // Borra la ventana
@@ -227,40 +252,40 @@ glBegin(GL_LINES);
 
     //Echeverria Valencia Greta Alitzel
     glColor3f (1.0, 0.2, 0.6 ); // Fija color rojo
-    letraE(columna[0] + 100,fila1);
-    letraV(columna[1] + 100,fila1);
-    letraG(columna[2] + 100,fila1);
-    letraA(columna[3] + 100,fila1);
+    letraE(columna[0] + 105,fila1);
+    letraV(columna[1] + 105,fila1);
+    letraG(columna[2] + 105,fila1);
+    letraA(columna[3] + 105,fila1);
 
-    //Gomez Almazan Martín
+    //Gomez Almazan Mart�n
     glColor3f (0.3,0.0,0.6 ); // Fija color rojo
-    letraG(columna[0],fila2);
-    letraA(columna[1],fila2);
-    letraM(columna[2],fila2);
+    letraG(columna[0]+15,fila2);
+    letraA(columna[1]+15,fila2);
+    letraM(columna[2]+15,fila2);
 
-    //Serafín Velazquez Andrea Lizeth
+    //Seraf�n Velazquez Andrea Lizeth
     glColor3f (0.34, 0.77, 0.8 ); // Fija color rojo
-    //letraS(columna[0] + 100,fila2);
-    letraV(columna[1] + 100,fila2);
-    letraA(columna[2] + 100,fila2);
-    letraL(columna[3] + 100,fila2);
+    letraS(columna[0] + 110,fila2);
+    letraV(columna[1] + 105,fila2);
+    letraA(columna[2] + 105,fila2);
+    letraL(columna[3] + 105,fila2);
 
     //Torres Gallardo Angel Gabriel
     glColor3f (0.0, 0.8, 0.8 ); // Fija color rojo
-    //letraT(columna[0],fila3);
+    letraT(columna[0],fila3);
     letraG(columna[1],fila3);
     letraA(columna[2],fila3);
     letraG(columna[3],fila3);
 
     //Vargas Hernandez Luis Abraham
     glColor3f (0.8, 0.2, 0.4 ); // Fija color rojo
-    letraV(columna[0] + 100,fila3);
-    letraH(columna[1] + 100,fila3);
-    letraL(columna[2] + 100,fila3);
-    letraA(columna[3] + 100,fila3);
+    letraV(columna[0] + 105,fila3);
+    letraH(columna[1] + 105,fila3);
+    letraL(columna[2] + 105,fila3);
+    letraA(columna[3] + 105,fila3);
 
 glEnd();
-glFlush(); // Procesa la rutina tan rápido
+glFlush(); // Procesa la rutina tan r�pido
 // como sea posible
 }
 int main(int argc, char *argv[]){
@@ -272,7 +297,7 @@ glutInitWindowSize (800, 600); // Establece alto y ancho de la
 // ventana
 glutCreateWindow ("Iniciales del equipo 2");
 Inicializa();
-glutDisplayFunc(Letras);
+glutDisplayFunc(LetrasIniciales);
 glutMainLoop();
 system("PAUSE");
 return EXIT_SUCCESS;
